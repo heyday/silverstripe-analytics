3d9caebe93d3c5b5aaf5d292ad76f8aac283c7c2
Lucas Frey <lucas.frey@heyday.co.nz>
2016-04-12 21:20:31 +1200

    Clean javascript implementation of tags

--- code/GoogleTagManagerProvider.php
+++ code/GoogleTagManagerProvider.php
@@ -27,11 +27,11 @@ class GoogleTagManagerProvider extends AnalyticsProvider
         $analyticsCode = <<< EOS
             <!-- Google Tag Manager -->
 			<noscript><iframe src="//www.googletagmanager.com/ns.html?id=$id height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
-			new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
-			\'//www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
-			})(window,document,\'script\',\'dataLayer\',\'$id\');</script>
+			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','$id');</script>
 			<!-- End Google Tag Manager -->
 EOS;
 
@@ -87,7 +87,7 @@ EOS;
      */
     private static function buildDataLayer()
     {
-        $data = array();
+        $data = [];
         foreach (self::$dataLayers as $key => $value) {
             $data[] .= "'" . $key . "' : '" . $value . "'";
         }

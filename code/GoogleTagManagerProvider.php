32355daae1444c0213e8295345463fe23e3c9c20
Lucas Frey <lucas.frey@heyday.co.nz>
2016-05-27 12:25:51 +1200

    Fix a typo on the Tag Manager provider. The module was working fine because browsers are cleaning HTML.

--- code/GoogleTagManagerProvider.php
+++ code/GoogleTagManagerProvider.php
@@ -24,7 +24,7 @@ class GoogleTagManagerProvider extends AnalyticsProvider
 
         $analyticsCode = <<< EOS
             <!-- Google Tag Manager -->
-			<noscript><iframe src="//www.googletagmanager.com/ns.html?id=$id height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+			<noscript><iframe src="//www.googletagmanager.com/ns.html?id=$id" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 			<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
